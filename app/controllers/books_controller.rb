dd3d679075dab5fde795b99d04a670cf1baa2818
Jack Reuter <djreuter45@gmail.com>
2017-11-21 13:40:08 -0700

    added new and create methods to books controller and added new book view template

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,19 @@
 class BooksController < ApplicationController
     def index
     end
+
+# define create and new methods for creating new book 
+    def new
+        @book = Book.new
+    end
+
+    def create
+        @book = Book.new(book_params)
+    end
+
+    # define information user will fill out
+    private 
+        def book_params
+            params.require(:book).permit(:title, :description, :author)
+        end
 end
