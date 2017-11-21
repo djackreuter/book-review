5ed2ca1893b9566674d3b76f763b0c34231279e7
Jack Reuter <djreuter45@gmail.com>
2017-11-21 14:52:20 -0700

    updated methods for showing books and added books to index view

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,14 @@
 class BooksController < ApplicationController
+
+# define before action to run before any other methods are used
+    before_action :find_book, only: [:show, :edit, :update, :destroy]
+
+# get all books in model and order by created at
     def index
+        @books = Book.all.order("created_at DESC")
+    end
+
+    def show
     end
 
 # define create and new methods for creating new book 
@@ -9,11 +18,21 @@ class BooksController < ApplicationController
 
     def create
         @book = Book.new(book_params)
+        if @book.save
+            redirect_to root_path
+        else
+            render 'new'
+        end
     end
 
     # define information user will fill out
     private 
         def book_params
             params.require(:book).permit(:title, :description, :author)
+        end
+
+        # corrasponds to the id of the book that is clicked on
+        def find_book
+            @book = Book.find(params[:id])
         end
 end
