7b408c288696e7419a69c5b561d7589e74b615df
Jack Reuter <djreuter45@gmail.com>
2017-11-24 13:32:12 -0700

    added method to add categories when creating a new book

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -14,12 +14,15 @@ class BooksController < ApplicationController
 # define create and new methods for creating new book 
 # Builds out books from current user
 # replaced Book.new
+# @categories adds a way to access categories when user creates a new book
     def new
         @book = current_user.books.build
+        @categories = Category.all.map{ |c| [c.name, c.id] }
     end
 
     def create
         @book = current_user.books.build(book_params) 
+        @book.category_id = params[:category_id]
         if @book.save
             redirect_to root_path
         else
@@ -47,7 +50,7 @@ class BooksController < ApplicationController
     # define information user will fill out
     private 
         def book_params
-            params.require(:book).permit(:title, :description, :author)
+            params.require(:book).permit(:title, :description, :author, :category_id)
         end
 
         # corrasponds to the id of the book that is clicked on
