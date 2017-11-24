6cb78934dff30325c287831e19e8cf7d827b31dd
Jack Reuter <djreuter45@gmail.com>
2017-11-24 15:10:17 -0700

    added category dropdown to navbar and updated index method to filter books based on category selected

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,7 +5,14 @@ class BooksController < ApplicationController
 
 # get all books in model and order by created at
     def index
-        @books = Book.all.order("created_at DESC")
+        # if catagory params are blank, show all books
+        if params[:category].blank?
+            @books = Book.all.order("created_at DESC")
+        else
+            # get id of category selected from category dropdown
+            @category_id = Category.find_by(name: params[:category]).id
+            @books = Book.where(:category_id => @category_id).order("created_at DESC")
+        end
     end
 
     def show
