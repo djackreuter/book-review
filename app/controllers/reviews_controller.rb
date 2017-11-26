85e0e6070e2e28ab4b7f16a1f6ee80731ff43717
Jack Reuter <djreuter45@gmail.com>
2017-11-26 14:51:36 -0700

    added edit and update methods to reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,7 @@
 class ReviewsController < ApplicationController
     # make @book accessable 
     before_action :find_book
+    before_action :find_review, only: [:edit, :update, :destroy]
 
     def new
         @review = Review.new
@@ -8,7 +9,7 @@ class ReviewsController < ApplicationController
 
     def create
         @review = Review.new(review_params)
-        
+
         # associate review with current book and current user
         @review.book_id = @book.id
         @review.user_id = current_user.id
@@ -17,6 +18,21 @@ class ReviewsController < ApplicationController
             redirect_to book_path(@book)
         else
             render 'new'
+        end
+    end
+
+    def edit
+        # dont need this line here anymore because the method was defined and called by before_action
+        # @review = Review.find(params[:id])
+    end
+
+    def update
+        @review = Review.find(params[:id])
+
+        if @review.update(review_params)
+            redirect_to book_path(@book)
+        else
+            render 'edit'
         end
     end
 
@@ -30,4 +46,8 @@ class ReviewsController < ApplicationController
             # finds current book view is associated with based on book_id
             @book = Book.find(params[:book_id])
         end
+
+        def find_review
+            @review = Review.find(params[:id])
+        end            
 end
