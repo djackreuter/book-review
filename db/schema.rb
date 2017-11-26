467536d745c0ca4b9309d35192b62a23adfbcc39
Jack Reuter <djreuter45@gmail.com>
2017-11-26 10:51:06 -0700

    updated migrations to add book_id to reviews table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171126174524) do
+ActiveRecord::Schema.define(version: 20171126175031) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 20171126174524) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.integer "book_id"
   end
 
   create_table "users", force: :cascade do |t|
